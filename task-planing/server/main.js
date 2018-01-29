6d6b6b1075f4a2c759edf9fce1e2eecafd4da31d
Ex <sirawit0676@gmail.com>
2018-01-29 21:09:27 +0700

    added plus.js for send parameter to plus in server side

--- task-planing/server/main.js
+++ task-planing/server/main.js
@@ -1,7 +1,8 @@
 //import './../imports/utils';
 
-import {User,name} from './../imports/utils';
+import {User} from './../imports/utils';
+import {add} from './../imports/plus';
 
 console.log('Log from /server/main.js');
 console.log(User()); 
-console.log(name);
+console.log(add(1,2));
