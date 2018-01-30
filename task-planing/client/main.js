039a0d80e070947016290998e15808701b42abb1
Ex <sirawit0676@gmail.com>
2018-01-30 20:16:28 +0700

    added react and react-dom

--- task-planing/client/main.js
+++ task-planing/client/main.js
@@ -1,8 +1,16 @@
 //import './../imports/utils';
 
-import def,{User,name} from './../imports/utils';
+//import def,{User,name} from './../imports/utils';
 
-console.log('log from client/main.js');
+/*console.log('log from client/main.js');
 console.log(User());
 console.log(name);
-console.log(def);
+console.log(def);*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+
+Meteor.startup(function (){
+
+});
