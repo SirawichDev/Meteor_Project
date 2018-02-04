ef77ffa12a9cf6b089ce2d0ba656df33a21b57f2
Ex <sirawit0676@gmail.com>
2018-02-04 22:55:42 +0700

    import meteor and User.js

--- task-planing/server/main.js
+++ task-planing/server/main.js
@@ -1,8 +1,18 @@
 //import './../imports/utils';
 
-import {User} from './../imports/utils';
+/*import {User} from './../imports/utils';
 import add from './../imports/plus';
 
 console.log('Log from /server/main.js');
 console.log(User()); 
-console.log(add(1,2));
+console.log(add(1,2));*/
+
+import {Meteor} from 'meteor/meteor';
+import {Users} from './../imports/api/user';
+
+Meteor.startup(function (){
+
+    
+});
+
+
